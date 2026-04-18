3c7597f2ccd6b92ba8d18e07b966ee13b4e47d28
Madrid <Lee>
2026-04-18 17:00:24 +0800

    feat: update navigation sections and remove GitHub ribbon

--- scripts/theme-override.js
+++ scripts/theme-override.js
@@ -8,9 +8,28 @@ hexo.extend.filter.register('before_generate', function() {
   }
 
   themeConfig.avatar = 'https://github.com/MadridLee.png';
+  themeConfig.menu = {
+    '主页': '/',
+    '技术贴': '/tech/',
+    '出去玩': '/travel/',
+    '随手记': '/jottings/',
+    '乱七八糟': '/misc/'
+  };
   themeConfig.subNav = [
     { github: 'https://github.com/MadridLee' },
     { jianshu: 'https://www.jianshu.com/u/0fb1be47e89d' }
   ];
+  themeConfig.open_in_new = {
+    article: true,
+    menu: false,
+    subNav: true
+  };
+  themeConfig.smart_menu = {
+    innerArchive: '所有文章',
+    friends: false,
+    aboutme: '关于我'
+  };
+  themeConfig.friends = {};
+  themeConfig.github = false;
   themeConfig.aboutme = '你好，我是 MadridLee。<br>这里记录技术、产品与生活思考。<br>欢迎通过 GitHub 和简书交流。';
 });
